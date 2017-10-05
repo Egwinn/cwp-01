d14802f755e36f35a325906353cd04c8f69ae456
Marina <dubovik.m14@gmail.com>
2017-10-05 20:45:28 +0200

    print all command line arguments

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const name = process.argv[2];
-console.log(`Hi ${name}!`);
+// task02
+//console.log('Hello World');
+
+// task03
+//const name = process.argv[2];
+//console.log(`Hi ${name}!`);
+
+// task04
+process.argv.forEach((val, index) => {
+    if(index > 1) console.log(`${index}: ${val}`);
+});
