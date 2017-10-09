6da0cfc5952f129bba1321d858160dc2943fdfce
Marina <dubovik.m14@gmail.com>
2017-10-10 00:01:01 +0200

    new function to copy files

--- index.js
+++ index.js
@@ -29,17 +29,18 @@ const sum = fs.createWriteStream('summary.js');
 let array_of_files = [];
 let copyright = "";
 
+// разбор содержимого директория
 let readDirectory = function(dir, prefix) {
     fs.readdir(dir, (err, files) => {
         if(err) {
-            console.error(err);
+            console.error("Невозможно прочитать содержимое директория " + dir);
         } else {
             files.forEach(function(element) {
-                let new_dir = dir + '/' + element;
-                if(fs.statSync(new_dir).isDirectory()) {
-                    readDirectory(new_dir, prefix + element + '/');
+                let new_unit = dir + '\\' + element;
+                if(fs.statSync(new_unit).isDirectory()) {
+                    readDirectory(new_unit, prefix + element + '/');
                 } else {
-                    array_of_files.push(new_dir);
+                    array_of_files.push(new_unit);
                     sum.write('console.log(\'' + prefix + element + '\');\n');
                 }
             }, this);
@@ -49,16 +50,32 @@ let readDirectory = function(dir, prefix) {
 
 readDirectory(DIR_PATH, prefix);
 
+// получение copyright
 fs.readFile("config.json", (err, data) => {
     if (err) console.error(err)
-    else {
-        copyright = JSON.parse(data).copyright;
-        let new_directory = DIR_PATH + '\\' + path.basename(DIR_PATH);
-        fs.access(new_directory, (err) => {
-            if(err && err.code == 'ENOENT')
-                fs.mkdir(new_directory, (err) => {
-                    console.error(err);
-                })
-        })
-    }
+    else copyright = JSON.parse(data).copyright;
 })
+let new_directory = DIR_PATH + '\\' + path.basename(DIR_PATH);
+// создание нового директория
+fs.access(new_directory, (err) => {
+    if(err && err.code == 'ENOENT') {
+        fs.mkdir(new_directory, (err) => {
+            if (err) console.error(err);
+        })
+        // копирование файлов с добавлением copyright
+        for (let i = 0; i < array_of_files.length; i++) {
+            let new_file = new_directory + '\\' + path.basename(array_of_files[i]);
+            const logger = fs.createWriteStream(new_file);
+            fs.readFile(array_of_files[i], (err, data) => {
+                if(err) console.error(err)
+                else logger.write(copyright + '\n\n--------\n' + data + '\n--------\n\n' + copyright);
+            })
+        }
+    }
+    else console.log("Такой директорий уже существует!");
+})
+
+// fs.watch(new_directory, (err, file) => {
+//     if(err) console.error(err)
+//     else console.log(file.toString());
+// })
