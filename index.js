788d613cf457fc8eca359076d24eb1176df2e8d9
Marina <dubovik.m14@gmail.com>
2017-10-09 22:37:48 +0200

    new function to create new directory

--- index.js
+++ index.js
@@ -14,24 +14,51 @@
 const path = require('path');
 const fs = require('fs');
 
-const DIR_PATH = process.argv[2];
-let prefix ="";
-const sum = fs.createWriteStream("summary.js");
+if (process.argv.length < 3) {
+    console.log("Недостаточно аргументов");
+    process.exit();
+}
+if (process.argv.length > 3) {
+    console.log("Слишком много аргументов");
+    process.exit();
+}
 
-let readDirectory = function(path, prefix) {
-    fs.readdir(path, (err, files) => {
-        if(err) console.error(err);
-        else {
+const DIR_PATH = process.argv[2];
+let prefix = "";
+const sum = fs.createWriteStream('summary.js');
+let array_of_files = [];
+let copyright = "";
+
+let readDirectory = function(dir, prefix) {
+    fs.readdir(dir, (err, files) => {
+        if(err) {
+            console.error(err);
+        } else {
             files.forEach(function(element) {
-                let new_dir = path + "/" + element;
+                let new_dir = dir + '/' + element;
                 if(fs.statSync(new_dir).isDirectory()) {
-                    readDirectory(new_dir, prefix + "/" + element);
+                    readDirectory(new_dir, prefix + element + '/');
                 } else {
-                    sum.write('console.log(\'' + prefix + '/' + element +'\');\n');
+                    array_of_files.push(new_dir);
+                    sum.write('console.log(\'' + prefix + element + '\');\n');
                 }
             }, this);
         }
     })
 }
 
-readDirectory(DIR_PATH,prefix);
+readDirectory(DIR_PATH, prefix);
+
+fs.readFile("config.json", (err, data) => {
+    if (err) console.error(err)
+    else {
+        copyright = JSON.parse(data).copyright;
+        let new_directory = DIR_PATH + '\\' + path.basename(DIR_PATH);
+        fs.access(new_directory, (err) => {
+            if(err && err.code == 'ENOENT')
+                fs.mkdir(new_directory, (err) => {
+                    console.error(err);
+                })
+        })
+    }
+})
